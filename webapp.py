f0e989a8e709ef2857843cd1429b06d9c6f4fd9a
Morgan <m.garricks@lancaster.ac.uk>
2024-10-28 16:45:07 +0000

    implemented completing todos

--- webapp.py
+++ webapp.py
@@ -1,18 +1,26 @@
 import streamlit as st
 import functions
 
+todos = functions.get_todos()
+
 def add_todo():
     todo = st.session_state["input_box"]+"\n"
     todos.append(todo)
     functions.set_todos(todos)
+    
 
-todos = functions.get_todos()
+
 st.title("my todo app")
 st.subheader("this is my todo app")
 st.write("This app helps productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.set_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="Enter a todo:",placeholder="New todo...", 
               on_change=add_todo, key="input_box") 
